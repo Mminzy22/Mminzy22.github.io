3eb0a4b47bfda85aabf3d1cd1b996a12871b5dab
Mminzy22 <aw7022@gmail.com>
2025-04-09 10:16:34 +0900

    fix: goatcounter 오류 수정

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -34,7 +34,9 @@ const swconf = {
 
       {%- comment -%} URLs containing the following prefixes will not be cached. {%- endcomment -%}
       urlPrefixes: [
-        'https://mminzy22.goatcounter.com/counter/'
+        {% if site.analytics.goatcounter.id != nil and site.pageviews.provider == 'goatcounter' %}
+          'https://{{ site.analytics.goatcounter.id }}.goatcounter.com/counter/'
+        {% endif %}
       ]
     },
 
